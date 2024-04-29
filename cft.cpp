362e63801010018f9152953ee4686ebcd9bb24bd
longwatermelon <longwatermelon1@gmail.com>
2024-04-28 19:50:55 -0700

    first commit

---
+++ cft.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define sig(x) (x<0?-1:1)
+template <typename T> T cdiv(T a, T b) {return a/b+(a/b>=0 && a%b!=0);}
+template <typename T> T fdiv(T a, T b) {return a/b-(a/b<0 && a%b!=0);}
+template <typename T> T mod(T a, T b) {return ((a%b)+b)%b;}
+template <typename T> void vprint(T st, T nd) {auto it=st;while (next(it)!=nd){cout<<*it<<' ';++it;}cout<<*it<<'\n';}
+
+void solve() {
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
