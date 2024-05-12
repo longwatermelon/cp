2c50665cca249a04938241fb81d2d18d57d17047
longwatermelon <longwatermelon1@gmail.com>
2024-05-11 17:34:32 -0700

    sz

--- cfth.cpp
+++ cfth.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 using ll=long long;
 #define sig(x) (x<0?-1:1)
+#define sz(x) ((int)size(x))
 template <typename T> T cdiv(T a, T b) {return a/b+(a/b>=0 && a%b!=0);}
 template <typename T> T fdiv(T a, T b) {return a/b-(a/b<0 && a%b!=0);}
 template <typename T> T mod(T a, T b) {return ((a%b)+b)%b;}
