0539a42c7eafc96a6e4b33e58c5afd7113b8e984
longwatermelon <longwatermelon1@gmail.com>
2024-05-11 17:35:48 -0700

    update

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cp cpt.cpp ~/templates/cpt.cpp
+cp cft.cpp ~/templates/cft.cpp
+cp cpth.cpp ~/templates/cpth.cpp
+cp cfth.cpp ~/templates/cfth.cpp
+cp -r cf ~/templates/cf
+cp -r usaco ~/templates/usaco
