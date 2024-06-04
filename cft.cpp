95ecd832f86dfeaaee461daef45cac025d1ac1ea
longwatermelon <longwatermelon1@gmail.com>
2024-06-04 15:18:47 -0700

    pt_t

--- cft.cpp
+++ cft.cpp
@@ -7,6 +7,7 @@ template <typename T> T cdiv(T a, T b) {return a/b+(a/b>=0 && a%b!=0);}
 template <typename T> T fdiv(T a, T b) {return a/b-(a/b<0 && a%b!=0);}
 template <typename T> T mod(T a, T b) {return ((a%b)+b)%b;}
 template <typename T> void vprint(T st, T nd) {auto it=st;while (next(it)!=nd){cout<<*it<<' ';it=next(it);}cout<<*it<<'\n';}
+struct pt_t {int x,y;};
 
 void solve() {
 }
