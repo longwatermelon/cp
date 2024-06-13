682f976659d359b290309c77ba83470615068b16
longwatermelon <longwatermelon1@gmail.com>
2024-06-13 13:03:53 -0700

    update

---
+++ usaco/2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define sig(x) (x<0?-1:1)
+#define sz(x) ((int)size(x))
+template <typename T> T cdiv(T a, T b) {return a/b+(a/b>=0 && a%b!=0);}
+template <typename T> T fdiv(T a, T b) {return a/b-(a/b<0 && a%b!=0);}
+template <typename T> T mod(T a, T b) {return ((a%b)+b)%b;}
+template <typename T> void vprint(T st, T nd) {auto it=st;while (next(it)!=nd){cout<<*it<<' ';it=next(it);}cout<<*it<<'\n';}
+struct pt_t {int x,y;bool operator<(pt_t b)const{return x==b.x?y<b.y:x<b.x;}bool operator==(pt_t b)const{return x==b.x&&y==b.y;}};
+void set_io(const char *name) {freopen((string(name)+".in").c_str(),"r",stdin);freopen((string(name)+".out").c_str(),"w",stdout);}
+
+void solve() {
+}
+
+int main() {
+    /* set_io(""); */
+}
