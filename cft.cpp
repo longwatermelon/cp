4eac40b1a973770dc18b17dbb98b1c8e08cff07b
longwatermelon <longwatermelon1@gmail.com>
2024-07-09 10:05:50 -0700

    update

--- cft.cpp
+++ cft.cpp
@@ -11,6 +11,8 @@ template <typename T> void vprint(T st, T nd) {auto it=st;while (next(it)!=nd){c
 #define PTY y
 struct pt_t {int PTX,PTY;bool operator<(pt_t pt2)const{return PTX==pt2.PTX?PTY<pt2.PTY:PTX<pt2.PTX;}bool operator==(pt_t pt2)const{return PTX==pt2.PTX&&PTY==pt2.PTY;}bool operator>(pt_t pt2)const{return !(*this<pt2||*this==pt2);}};
 template <typename T> vector<int> genord(int len, T cmp) {vector<int> ord(len+1);for(int i=1;i<=len;++i)ord[i]=i;sort(begin(ord)+1,end(ord),cmp);return ord;}
+int M=1e9+7;
+int modinv(int x) {return x<=1 ? x : M-M/x*modinv(M%x)%M;}
 
 void solve() {
 }
