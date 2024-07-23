5a187d79786c6282e4d9e32d41bf23063efe38bc
longwatermelon <longwatermelon1@gmail.com>
2024-07-23 16:03:04 -0700

    update:

--- cpt.cpp
+++ cpt.cpp
@@ -3,8 +3,8 @@ using namespace std;
 using ll=long long;
 #define sig(x) (x<0?-1:1)
 #define sz(x) ((int)size(x))
-template <typename T> T cdiv(T a, T b) {return a/b+(a/b>=0 && a%b!=0);}
-template <typename T> T fdiv(T a, T b) {return a/b-(a/b<0 && a%b!=0);}
+template <typename T> T cdiv(T a, T b) {return a/b+(sig(a)/sig(b)==1 && a%b);}
+template <typename T> T fdiv(T a, T b) {return a/b-(sig(a)/sig(b)==-1 && a%b);}
 template <typename T> T mod(T a, T b) {return ((a%b)+b)%b;}
 template <typename T> T mod1(T a, T b) {return (a-1)%b+1;}
 template <typename T> void vprint(T st, T nd) {auto it=st;while (next(it)!=nd){cout<<*it<<' ';it=next(it);}cout<<*it<<'\n';}
