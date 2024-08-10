5d6c92c2e6d1ce96e023f40cce72695dc2f164ca
longwatermelon <longwatermelon1@gmail.com>
2024-08-10 15:42:22 -0700

    update

---
+++ cf/F.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define sz(x) ((int)size(x))
+#define all(x) begin(x),end(x)
+#define all1(x) begin(x)+1,end(x)
+template <typename T> T cdiv(T a, T b) {return (a+b-1)/b;}
+
+void solve() {
+}
+
+int main() {
+    int _; cin>>_;
+    while (_--) {
+        solve();
+    }
+}
