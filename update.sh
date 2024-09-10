788e13c178d9dd832835f9cef7884a13479bf738
longwatermelon <longwatermelon1@gmail.com>
2024-09-10 16:57:00 -0700

    update

--- update.sh
+++ update.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 cp cpt.cpp ~/templates/cpt.cpp
 cp cft.cpp ~/templates/cft.cpp
-cp bt.cpp ~/templates/bt.cpp
 rm -rf ~/templates/cf
 rm -rf ~/templates/usaco
 cp -r cf ~/templates/cf
--- usaco/1.cpp
+++ usaco/1.cpp
@@ -1,1 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename T> using vec=vector<T>;
+template <typename T> struct vec2:vector<vector<T>> {vec2()=default;vec2(int n, int m, T val=T()):vector<vector<T>>(n,vector<T>(m,val)){}};
+template <typename T> struct vec3:vector<vector<vector<T>>> {vec3()=default;vec3(int n, int m, int k, T val=T()):vector<vector<vector<T>>>(n,vector<vector<T>>(m,vector<T>(k,val))){}};
+#define PTX x
+#define PTY y
+struct pt_t {int PTX,PTY;bool operator==(pt_t pt2)const{return PTX==pt2.PTX&&PTY==pt2.PTY;}};
 
+void solve() {
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+}
