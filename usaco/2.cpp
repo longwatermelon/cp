5bee4a4cda922981f695bfe0ec59ba893f5dbb46
longwatermelon <longwatermelon1@gmail.com>
2024-09-11 06:48:10 -0700

    update

--- usaco/2.cpp
+++ usaco/2.cpp
@@ -1,5 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll=long long;
+#define sig(x) (x<0?-1:1)
+#define sz(x) ((int)size(x))
+#define all(x) begin(x),end(x)
+#define all1(x) begin(x)+1,end(x)
+template <typename T> T cdiv(T a, T b) {return a/b+(sig(a)/sig(b)==1 && a%b);}
 template <typename T> using vec=vector<T>;
 template <typename T> struct vec2:vector<vector<T>> {vec2()=default;vec2(int n, int m, T val=T()):vector<vector<T>>(n,vector<T>(m,val)){}};
 template <typename T> struct vec3:vector<vector<vector<T>>> {vec3()=default;vec3(int n, int m, int k, T val=T()):vector<vector<vector<T>>>(n,vector<vector<T>>(m,vector<T>(k,val))){}};
