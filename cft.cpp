54503b61de5eff65f2f7ba51c55d8f1ef5b269ed
longwatermelon <jomathyc@gmail.com>
2024-10-16 14:53:30 -0700

    update

--- cft.cpp
+++ cft.cpp
@@ -20,14 +20,6 @@ struct pt_t {int PTX,PTY;bool operator==(pt_t pt2)const{return PTX==pt2.PTX&&PTY
 int MD=1e9+7;
 ll modpow(ll x, ll p) {assert(p>=0);return p==0?1:((ll)modpow(((ll)x*x)%MD,p/2)*(p&1?x:1))%MD;}
 ll modinv(ll x) {return modpow(x,MD-2);}
-ll rencr(int n,int r){if(n<r)return 0;else if(r==0||r==n) return 1;else return(rencr(n-1,r-1)+rencr(n-1,r))%MD;}
-vec<int> F,IF; // fac, inv fac
-void genfac(int mx) {F=vec<int>(mx+1);IF=vec<int>(mx+1);F[0]=1;IF[0]=modinv(1);for(int x=1;x<=mx;++x){F[x]=((ll)F[x-1]*x)%MD;IF[x]=modinv(F[x]);}}
-ll clncr(int n,int r){if(n<r)return 0;return(((ll)F[n]*IF[r])%MD*IF[n-r])%MD;}
-ll logfl(ll base, ll x) {assert(base>1);ll res=0, val=1;while (val*base<=x){val*=base;res++;}return res;}
-ll logcl(ll base, ll x) {assert(base>1);ll res=0, val=1;while (val<x){val*=base;res++;}return res;}
-ll sqrtfl(ll x) {ll rt=sqrtl(x);if (rt*rt>x) rt--;if ((rt+1)*(rt+1)<=x) rt++;return rt;}
-ll sqrtcl(ll x) {ll rt=sqrtl(x);if (rt*rt<x) rt++;if ((rt-1)*(rt-1)>=x) rt--;return rt;}
 
 void solve() {
 }
--- lib.cpp
+++ lib.cpp
@@ -5,7 +5,8 @@ using ll=long long;
 #define sz(x) ((int)size(x))
 #define all(x) begin(x),end(x)
 #define all1(x) begin(x)+1,end(x)
-
+const int MD=1e9+7;
+template <typename T> using vec=vector<T>;
 
 // sieve of eratosthenes, O(mx log(log mx))
 // determine if all numbers <=mx are prime
@@ -46,4 +47,67 @@ template <typename T> map<T,int> primefac(T x) {
 
     if (x>1) mp[x]++;
     return mp;
+}
+
+// log_{base}(x) floored
+ll logfl(ll base, ll x) {
+    assert(base>1);
+    ll res=0, val=1;
+    while (val*base<=x) {
+        val*=base;
+        res++;
+    }
+    return res;
+}
+
+// log_{base}(x) ceiling
+ll logcl(ll base, ll x) {
+    assert(base>1);
+    ll res=0, val=1;
+    while (val<x) {
+        val*=base;
+        res++;
+    }
+    return res;
+}
+
+// floor sqrt of x
+ll sqrtfl(ll x) {
+    ll rt=sqrtl(x);
+    if (rt*rt>x) rt--;
+    if ((rt+1)*(rt+1)<=x) rt++;
+    return rt;
+}
+
+// ceil sqrt of x
+ll sqrtcl(ll x) {
+    ll rt=sqrtl(x);
+    if (rt*rt<x) rt++;
+    if ((rt-1)*(rt-1)>=x) rt--;
+    return rt;
+}
+
+// recursive n choose r
+ll rencr(int n, int r) {
+    if (n<r) return 0;
+    else if (r==0||r==n) return 1;
+    else return (rencr(n-1,r-1)+rencr(n-1,r))%MD;
+}
+
+// closed form n choose r
+vec<int> F,IF; // fac, inv fac
+ll clncr(int n, int r) {
+    if (n<r) return 0;
+    return (((ll)F[n]*IF[r])%MD*IF[n-r])%MD;
+}
+
+// x^p in mod MD
+ll modpow(ll x, ll p) {
+    assert(p>=0);
+    return p==0 ? 1 : ((ll)modpow(((ll)x*x)%MD,p/2)*(p&1?x:1))%MD;
+}
+
+// modular inverse of x in mod MD
+ll modinv(ll x) {
+    return modpow(x,MD-2);
 }
