e65d4969ea7cd6ad36e8294409d386ca1a46227f
longwatermelon <jomathyc@gmail.com>
2025-01-04 17:26:27 -0700

    update

--- cft.cpp
+++ cft.cpp
@@ -10,10 +10,10 @@ template <typename T> bool ckmax(T &a, T b) {return b>a ? a=b, true : false;}
 template <typename T> void vprint(T st, T nd) {auto it=st;while (next(it)!=nd){cout<<*it<<' ';it=next(it);}cout<<*it<<'\n';}
 void yn(bool ans) {puts(ans?"yes":"no");}
 template <typename T> T cdiv(T a, T b) {return a/b+(sig(a)/sig(b)==1 && a%b);}
+ll logfl(ll base, ll x) {assert(base>1);ll res=0,val=1;while (val*base<=x) {val*=base;res++;}return res;}
 template <typename T> using vec=vector<T>;
 template <typename T> struct vec2:vector<vector<T>> {vec2()=default;vec2(int n, int m, T val=T()):vector<vector<T>>(n,vector<T>(m,val)){}};
 template <typename T> struct vec3:vector<vector<vector<T>>> {vec3()=default;vec3(int n, int m, int k, T val=T()):vector<vector<vector<T>>>(n,vector<vector<T>>(m,vector<T>(k,val))){}};
-template <typename T> void reset(T *arr, int l, int r, T val) {fill(arr+l,arr+r+1,val);}
 
 void solve() {
 }
