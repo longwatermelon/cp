5e7a302946bcf59a21f9aec25eb3a581be72e49d
longwatermelon <jomathyc@gmail.com>
2025-02-26 13:55:53 -0700

    update

--- cpt.cpp
+++ cpt.cpp
@@ -15,7 +15,6 @@ int popcount(ll x) {int cnt=0;while (x) {cnt+=x&1;x/=2;}return cnt;}
 template <typename T> using vec=vector<T>;
 template <typename T> struct vec2:vector<vector<T>> {vec2()=default;vec2(int n, int m, T val=T()):vector<vector<T>>(n,vector<T>(m,val)){}void assign(int n, int m, T val = T()) {this->vector<vector<T>>::assign(n, vector<T>(m, val));}};
 template <typename T> struct vec3:vector<vector<vector<T>>> {vec3()=default;vec3(int n, int m, int k, T val=T()):vector<vector<vector<T>>>(n,vector<vector<T>>(m,vector<T>(k,val))){}void assign(int n, int m, int k, T val = T()) {this->vector<vector<vector<T>>>::assign(n, vector<vector<T>>(m, vector<T>(k, val)));}};
-const int d4i[]={-1,0,1,0}, d4j[]={0,1,0,-1}, d8i[]={-1,-1,0,1,1,1,0,-1}, d8j[]={0,1,1,1,0,-1,-1,-1};
 
 void solve() {
 }
